05e55953ea8e62699cb034f13cf60b4fe0703731
BubenokGeorgy <102595170+BubenokGeorgy@users.noreply.github.com>
2023-06-22 22:27:33 +0300

    Delete config.py

--- exam/app/config.py
+++
@@ -1,9 +0,0 @@
-SECRET_KEY = 'dfsfdsfsdfsdfsdfsdfsdfsdfdsfsdfsdfdsfd'
-
-MYSQL_USER = 'std_2196_exam'
-MYSQL_PASSWORD = 'testexam'
-MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
-MYSQL_DATABASE = 'std_2196_exam'
-
-ADMINISTRATOR_ROLE_ID = 1
-MODERATOR_ROLE_ID = 2
